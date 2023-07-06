0e8ee04e5924b9ffa075b51f2020ad719f6a991a
CactiChameleon9 <51231053+CactiChameleon9@users.noreply.github.com>
2023-07-06 17:26:43 +0100

    Rearrange examples layout; Make cars multi-colored; Display best eval

--- addons/Godot AI Framework/examples/GeneticManagerExamples/CarsDriveForward/Car.gd
+++ addons/Godot AI Framework/examples/GeneticManagerExamples/CarsDriveForward/Car.gd
@@ -9,15 +9,19 @@ var front_wheel_throttle: float = 0
 var back_wheel_velocity: float = 0
 var back_wheel_throttle: float = 0
 
-var max_acceleration: float = 10.0
+var max_acceleration: float = 5.0
+
+
+func _ready():
+	$BodyTexture.color = Color(randf(), randf(), randf(), 0.7)
 
 
 func _physics_process(delta):
 	front_wheel_velocity += max_acceleration * front_wheel_throttle * delta
 	back_wheel_velocity += max_acceleration * back_wheel_throttle * delta
 	
-	$FrontWheel.angular_velocity += front_wheel_velocity * delta
-	$BackWheel.angular_velocity += front_wheel_velocity * delta
+	$FrontWheel.angular_velocity = front_wheel_velocity
+	$BackWheel.angular_velocity = front_wheel_velocity
 
 
 func _on_alive_timer_timeout():
