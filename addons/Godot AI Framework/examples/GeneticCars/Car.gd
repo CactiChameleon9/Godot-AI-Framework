c3324ac65d7583934c4806ae550ede067226598e
CactiChameleon9 <51231053+CactiChameleon9@users.noreply.github.com>
2023-07-06 17:06:21 +0100

    Fix the GeneticCar Example

--- addons/Godot AI Framework/examples/GeneticCars/Car.gd
+++ addons/Godot AI Framework/examples/GeneticCars/Car.gd
@@ -9,15 +9,16 @@ var front_wheel_throttle: float = 0
 var back_wheel_velocity: float = 0
 var back_wheel_throttle: float = 0
 
-var max_acceleration: float = 200.0
+var max_acceleration: float = 10.0
 
 
 func _physics_process(delta):
 	front_wheel_velocity += max_acceleration * front_wheel_throttle * delta
 	back_wheel_velocity += max_acceleration * back_wheel_throttle * delta
 	
-	$FrontWheel.rotation_degrees += front_wheel_velocity * delta
-	$BackWheel.rotation_degrees += front_wheel_velocity * delta
+	$FrontWheel.angular_velocity += front_wheel_velocity * delta
+	$BackWheel.angular_velocity += front_wheel_velocity * delta
+
 
 func _on_alive_timer_timeout():
 	emit_signal("finished", self)
